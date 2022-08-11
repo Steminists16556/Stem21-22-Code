5290af03229075c7e034fa236f37076f84a6b945
Steminists16556 <106121445+Steminists16556@users.noreply.github.com>
2022-08-11 16:34:46 -0500

    Create BlueAllianceAuto.java
    Blue Alliance Autonomous for FTC 2021-2022 season.

---
+++ BlueAllianceAuto.java
@@ -0,0 +1,104 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "BlueAllianceAuto (Blocks to Java)")
+public class BlueAllianceAuto extends LinearOpMode {
+
+  private DcMotor frontright;
+  private DcMotor frontleft;
+  private DcMotor backright;
+  private DcMotor backleft;
+  private DcMotor armMotor;
+  private Servo armServo;
+  //
+  private ElapsedTime runtime = new ElapsedTime();
+  
+  /**
+   * This function is executed when this Op Mode is selected from the Driver Station.
+   */
+  @Override
+  public void runOpMode() {
+    // Put initialization blocks here.
+    
+    waitForStart();
+    
+        
+    frontright = hardwareMap.get(DcMotor.class, "front right");
+    frontleft = hardwareMap.get(DcMotor.class, "front left");
+    backright = hardwareMap.get(DcMotor.class, "back right");
+    backleft = hardwareMap.get(DcMotor.class, "back left");
+    armMotor = hardwareMap.get(DcMotor.class, "armMotor");
+    armServo = hardwareMap.get(Servo.class, "armServo");
+    //direction of wheels
+    
+    frontright.setDirection(DcMotor.Direction.REVERSE);
+    frontleft.setDirection(DcMotor.Direction.REVERSE);
+    backright.setDirection(DcMotor.Direction.REVERSE);
+    backleft.setDirection(DcMotor.Direction.REVERSE);
+    //
+    armMotor.setDirection(DcMotor.Direction.REVERSE);
+    //
+    armServo.setPosition(0);
+    
+    
+    runtime.reset();
+    
+    armMotor.setPower(0.5);
+    frontright.setPower(0);
+    frontleft.setPower(0);
+    backright.setPower(0);
+    backleft.setPower(0);
+    //move backward
+    sleep(1000); //pauses before start
+    frontright.setPower(-0.5);
+    frontleft.setPower(0.5);
+    backright.setPower(0.5);
+    backleft.setPower(-0.5);
+    
+    armMotor.setPower(1);
+    
+    sleep(760); //time interval for going forward
+    
+    frontright.setPower(0);
+    frontleft.setPower(0);
+    backright.setPower(0);
+    backleft.setPower(0);
+    
+    sleep(1000); //pauses for the drop
+    armServo.setPosition(0.5);
+    sleep(500);
+    //move forward
+
+    
+    //power of the weels with direction 
+    frontright.setPower(0.5);
+    frontleft.setPower(-0.5);
+    backright.setPower(-0.5);
+    backleft.setPower(0.5);
+    
+    sleep(1050); //time interval for going backward
+    //move left
+    frontright.setPower(-0.5);
+    frontleft.setPower(-0.5);
+    backright.setPower(0.5);
+    backleft.setPower(0.5);
+    
+    armMotor.setPower(1);
+    
+    sleep(2550); //time interval for going sideways
+    frontright.setPower(0);
+    frontleft.setPower(0);
+    backright.setPower(0);
+    backleft.setPower(0);
+    
+       
+       
+      
+    
+  }
+}
